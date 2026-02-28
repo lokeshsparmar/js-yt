b900d0a22e52eb5e39d0c0a32189249fed830e19
lokeshsparmar <lokeshsinghparmar50@gmail.com>
2026-02-28 18:05:15 +0000

    dcon

--- 01_basics/03_DatatypeConversion.js
+++ 01_basics/03_DatatypeConversion.js
@@ -19,3 +19,26 @@ console.log(booleanIsLoggedIn);
 //1=>true;0=>false
 //""=>false
 //"lokesh"=>true
+
+let somenumber=33
+let somestring=String(somenumber)
+console.log(somestring)
+console.log(typeof somestring)
+
+//****************************operations******************************
+
+let value=3
+let Negvalue=-value
+console.log(Negvalue)
+
+console.log(2+2)
+console.log(2-2)
+console.log(2*2)
+console.log(2/2)
+console.log(2**4)
+console.log(41%3)
+
+let str1="hello"
+let str2="pratha"
+let str3=str1+str2
+console.log(str3)
