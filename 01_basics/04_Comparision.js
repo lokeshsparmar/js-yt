f7322085218ddf31a8e9b96e9ea8acecdd57c944
lokeshsparmar <lokeshsinghparmar50@gmail.com>
2026-03-07 18:27:22 +0000

    comparision

---
+++ 01_basics/04_Comparision.js
@@ -0,0 +1,20 @@
+console.log(2>1)
+console.log(2>=1)
+console.log(2==1)
+console.log(2<=1)
+console.log(2!=1)
+
+console.log("2">1)
+console.log("02">1)
+
+console.log(null>0)
+console.log(null==0)
+console.log(null>=0)//equality check == and comparison > < >= <= works differently bcz in comparision null is treated as 0 
+
+console.log(undefined==0)
+console.log(undefined>0)
+console.log(undefined<0)
+
+//"===" used for strict check it even checks for the data type
+console.log("2"===2)
+console.log(2===2)
