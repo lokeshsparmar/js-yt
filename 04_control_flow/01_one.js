9739a6b1b62cfb8fd9eb42c5cffb98ff41d989f5
lokeshsparmar <lokeshsinghparmar50@gmail.com>
2026-07-19 04:31:08 +0000

    control flow  (if else)

--- 04_control_flow/01_one.js
+++ 04_control_flow/01_one.js
@@ -1,5 +1,52 @@
 //if
- 
-if(true){
+const isLoggedIn=true
+const temperature=41
 
+if(temperature===40){
+    console.log("less than 50")
+}else{
+    console.log("temperature is greater than 50")
+}
+
+//console.log("Execute");
+//<,>,<=,>=,==,!=,===,!==
+
+const score=200
+
+if(score >100){
+    let power="fly"
+    console.log(`user power: ${power}`)
+}
+
+//console.log(`user power:${power}`) //will not execute bcz its out of scope
+
+const balance=1000
+
+if(balance>500) console.log("test1"),console.log("test2");
+
+if(balance<500){
+    console.log("less than 500")
+}else if(balance<750){
+    console.log("less than 750")
+}
+else if(balance<900){
+    console.log("less than 900")
+}
+
+else{
+    console.log("less than 1200")
+}
+
+
+const userLoggedIn=true
+const debitCard=true
+const loggedInFromGoogle=false
+const loggedInFromEmail=true
+
+if(userLoggedIn && debitCard &&2==2){
+    console.log("allowed to buy courses")
+}
+
+if(loggedInFromGoogle || loggedInFromEmail){
+    console.log("user logged in")
 }
