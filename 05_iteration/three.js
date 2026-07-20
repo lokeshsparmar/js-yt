acf55a7f226103d25c6ff33c3fd49b88c89452b6
lokeshsparmar <lokeshsinghparmar50@gmail.com>
2026-07-20 18:28:52 +0000

    notes

---
+++ 05_iteration/three.js
@@ -0,0 +1,31 @@
+//for of
+
+["","",""]
+[{}, {}, {}]  //objects
+
+const arr=[1,2,3,4,5]
+
+// for(const i of arr){    
+//     console.log(i)
+// }
+
+// const greetings ="hello world!"
+// for(const greet of greetings){
+//     if(greet==" "){
+//         continue;
+//     }
+//     console.log(`each char is ${greet}`)   
+// }
+
+//maps //unique values
+
+const map=new Map()
+map.set('IN',"India")
+map.set('USA',"united states of america")
+
+//console.log(map);
+
+for(const i of map){
+    console.log(i);
+    
+}
